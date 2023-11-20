94d4f1f51d05d8781f050143c06e701937ddcce4
Alex M - laptop <vootehdoo@gmail.com>
2023-11-20 11:29:11 +0100

    Remove the 1 function from both listeners

--- scripts/script.js
+++ scripts/script.js
@@ -7,193 +7,73 @@ const cityNameContainer = document.querySelector('.city-info');
 const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
 
-// add eventlistener to input field
-inputField.addEventListener('keyup', function (event) {
-	// get the current value after the user submitted the city name
-	const theNameOfTheCity = document.querySelector("#city-name").value;
-
-	// check if the keyup action is used on an Enter key
-	if (event.code === "Enter") {
-		// Make the api call to get the weather Data based on the City
-		fetch("http://api.weatherapi.com/v1/forecast.json?key=" + API.key + "&q=" + theNameOfTheCity + "&days=7&aqi=no&alerts=no")
-			// Transform the response in a readable javascript format
-			.then(response => response.json())
-			// final formatted data from the API call
-			.then(data => {
-				// Check if data is received
-				console.log(data);
-
-
-				// check if the data is not giving back an error
-				if (data.error) {
-					// stop the event from continuing the code if there is an error
-					return alert("Hey are you sure you are not holding up your map upside down?");
-					console.log("check if code stops");
-				} else {
-					// continue with the code if there are no errors
-					const container = document.querySelector(".container");
-					// Remove existing children if there are any in the <element class="container">
-					while (container.lastChild) {
-						container.removeChild(container.lastChild);
-					};
-
-					// I also found this option to remove the children (but it removes all html content though)
-					container.innerHTML = "";
-
-					// container.children.forEach(child => {
-					//     container.remove(child);
-					// })
-
-					// Display the location in the browser as "City, Country"
-					cityNameContainer.textContent = data.location.name + ", " + data.location.country;
-
-					// Create cards for each days (first 5 days) of the week.
-					// if I want to have 7 days, I just need to augment the number in the loop condition from 5 to 7
-					for (let i = 0; i < 5; i++) {
-
-						// get the container again for add the cards
-						const div = document.querySelector('.container');
-
-						// d = date
-						const d = new Date();
-						// console.log(weekdays[(d.getDay() + i) % 7])
-						// dow = dateOfWeek
-						const dow = weekdays[(d.getDay() + i) % 7];
-
-						// Create the elements with Data
-						const card = document.createElement('div');
-						card.classList.add("card");
-
-						// if it's the first element (index === 0), add a second class: "main-card" for unique styling
-						if (i === 0) card.classList.add("main-card");
-
-						div.appendChild(card);
-
-						const initialContentBeforeSlideAnimation = document.createElement('div');
-						initialContentBeforeSlideAnimation.classList.add("imgBx");
-						card.appendChild(initialContentBeforeSlideAnimation);
-
-
-						const cardImg = document.createElement('img');
-						cardImg.src = data.forecast.forecastday[i].day.condition.icon;
-						cardImg.alt = "Icon describing the following weather: " + data.forecast.forecastday[i].day.condition.text;
-						initialContentBeforeSlideAnimation.appendChild(cardImg);
-
-
-
-
-						const contentBox = document.createElement("div");
-						contentBox.classList.add("contentBx");
-						card.appendChild(contentBox);
-
-						const dowContentBeforeSliderAnimation = document.createElement("h2");
-						dowContentBeforeSliderAnimation.innerHTML = dow;
-						contentBox.appendChild(dowContentBeforeSliderAnimation);
-
-						console.log(data.forecast.forecastday[i].day.condition.text);
-						const tempDescription = document.createElement("h4");
-						tempDescription.innerHTML = data.forecast.forecastday[i].day.condition.text;
-						contentBox.appendChild(tempDescription);
-
-						const currentTempBox = document.createElement("div");
-						currentTempBox.classList.add("color");
-						contentBox.appendChild(currentTempBox);
-
-						const currentTempHeader = document.createElement("h3");
-						currentTempHeader.innerHTML = "Temp:";
-						currentTempBox.appendChild(currentTempHeader);
-
-						const currentT = document.createElement("span");
-						currentT.classList.add("current-temp");
-
-						// OLD structure from different API
-						// let averageTemp = (result.daily.temperature_2m_min[i] + result.daily.temperature_2m_max[i]) / 2;
-						// if(i === 0) averageTemp = result.current.temperature_2m;
-
-						// NEW structure:
-						currentT.innerHTML = data.forecast.forecastday[i].day.avgtemp_c + "°C";
-						currentTempBox.appendChild(currentT);
-
-						const minMax = document.createElement("div");
-						minMax.classList.add("details");
-						contentBox.appendChild(minMax);
-
-						const minMaxTempHeader = document.createElement("h3");
-						minMaxTempHeader.innerHTML = "More:";
-						minMax.appendChild(minMaxTempHeader);
-
-						const minT = document.createElement("span");
-						minT.classList.add("min-temp");
-						minT.innerHTML = data.forecast.forecastday[i].day.mintemp_c + "°C";
-						minMax.appendChild(minT);
-
-						const maxT = document.createElement("span");
-						maxT.classList.add("max-temp");
-						maxT.innerHTML = data.forecast.forecastday[i].day.maxtemp_c + "°C";
-						minMax.appendChild(maxT);
-					}
-				}
-			}).catch(err => {
-				//not working
-				// alert("Hey are you sure you are not holding up your map upside down?")
-			});
-	}
-});
-
-// add eventlistener to buttonElement
-buttonElement.addEventListener('click', function () {
-	const theNameOfTheCity = document.querySelector("#city-name").value;
-	console.log("clicked");
-	fetch("http://api.weatherapi.com/v1/forecast.json?key=" + API.key + "&q=" + theNameOfTheCity + "&days=7&aqi=no&alerts=no")
+async function startWeatherApp() {
+	fetch("http://api.weatherapi.com/v1/forecast.json?key=" + API.key + "&q=" + inputField.value + "&days=7&aqi=no&alerts=no")
 		.then(response => response.json())
 		.then(data => {
-			console.log(data);
 			if (data.error) {
 				return alert("Hey are you sure you are not holding up your map upside down?");
-				console.log("check if code stops");
+
 			} else {
+				// continue with the code if there are no errors
 				const container = document.querySelector(".container");
+				// Remove existing children if there are any in the <element class="container">
 				while (container.lastChild) {
 					container.removeChild(container.lastChild);
 				};
 
+				// I also found this option to remove the children (but it removes all html content though)
 				container.innerHTML = "";
+
 				// container.children.forEach(child => {
 				//     container.remove(child);
 				// })
 
+				// Display the location in the browser as "City, Country"
 				cityNameContainer.textContent = data.location.name + ", " + data.location.country;
 
+				// Create cards for each days (first 5 days) of the week.
+				// if I want to have 7 days, I just need to augment the number in the loop condition from 5 to 7
 				for (let i = 0; i < 5; i++) {
-					const container = document.querySelector('.container');
 
-					const date = new Date();
-					// console.log(weekdays[(date.getDay() + i) % 7])
-					const dayOfTheWeek = weekdays[(date.getDay() + i) % 7];
+					// get the container again for add the cards
+					const div = document.querySelector('.container');
+
+					// d = date
+					const d = new Date();
+					// console.log(weekdays[(d.getDay() + i) % 7])
+					// dow = dateOfWeek
+					const dow = weekdays[(d.getDay() + i) % 7];
 
 					// Create the elements with Data
 					const card = document.createElement('div');
 					card.classList.add("card");
 
+					// if it's the first element (index === 0), add a second class: "main-card" for unique styling
 					if (i === 0) card.classList.add("main-card");
 
-					container.appendChild(card);
+					div.appendChild(card);
 
-					const imageBox = document.createElement('div');
-					imageBox.classList.add("imgBx");
-					card.appendChild(imageBox);
+					const initialContentBeforeSlideAnimation = document.createElement('div');
+					initialContentBeforeSlideAnimation.classList.add("imgBx");
+					card.appendChild(initialContentBeforeSlideAnimation);
+
 
 					const cardImg = document.createElement('img');
 					cardImg.src = data.forecast.forecastday[i].day.condition.icon;
-					imageBox.appendChild(cardImg);
+					cardImg.alt = "Icon describing the following weather: " + data.forecast.forecastday[i].day.condition.text;
+					initialContentBeforeSlideAnimation.appendChild(cardImg);
+
+
+
 
 					const contentBox = document.createElement("div");
 					contentBox.classList.add("contentBx");
 					card.appendChild(contentBox);
 
-					const cardHeader = document.createElement("h2");
-					cardHeader.innerHTML = dayOfTheWeek;
-					contentBox.appendChild(cardHeader);
+					const dowContentBeforeSliderAnimation = document.createElement("h2");
+					dowContentBeforeSliderAnimation.innerHTML = dow;
+					contentBox.appendChild(dowContentBeforeSliderAnimation);
 
 					console.log(data.forecast.forecastday[i].day.condition.text);
 					const tempDescription = document.createElement("h4");
@@ -208,40 +88,51 @@ buttonElement.addEventListener('click', function () {
 					currentTempHeader.innerHTML = "Temp:";
 					currentTempBox.appendChild(currentTempHeader);
 
-					const currentTemp = document.createElement("span");
-					currentTemp.classList.add("current-temp");
+					const currentT = document.createElement("span");
+					currentT.classList.add("current-temp");
 
 					// OLD structure from different API
 					// let averageTemp = (result.daily.temperature_2m_min[i] + result.daily.temperature_2m_max[i]) / 2;
 					// if(i === 0) averageTemp = result.current.temperature_2m;
-					currentTemp.innerHTML = data.forecast.forecastday[i].day.avgtemp_c + "°C";
-					currentTempBox.appendChild(currentTemp);
 
-					const minMaxTemperatures = document.createElement("div");
-					minMaxTemperatures.classList.add("details");
-					contentBox.appendChild(minMaxTemperatures);
+					// NEW structure:
+					currentT.innerHTML = data.forecast.forecastday[i].day.avgtemp_c + "°C";
+					currentTempBox.appendChild(currentT);
+
+					const minMax = document.createElement("div");
+					minMax.classList.add("details");
+					contentBox.appendChild(minMax);
 
 					const minMaxTempHeader = document.createElement("h3");
 					minMaxTempHeader.innerHTML = "More:";
-					minMaxTemperatures.appendChild(minMaxTempHeader);
+					minMax.appendChild(minMaxTempHeader);
 
-					const minTemp = document.createElement("span");
-					minTemp.classList.add("min-temp");
-					minTemp.innerHTML = data.forecast.forecastday[i].day.mintemp_c + "°C";
-					minMaxTemperatures.appendChild(minTemp);
+					const minT = document.createElement("span");
+					minT.classList.add("min-temp");
+					minT.innerHTML = data.forecast.forecastday[i].day.mintemp_c + "°C";
+					minMax.appendChild(minT);
 
-					const maxTemp = document.createElement("span");
-					maxTemp.classList.add("max-temp");
-					maxTemp.innerHTML = data.forecast.forecastday[i].day.maxtemp_c + "°C";
-					minMaxTemperatures.appendChild(maxTemp);
+					const maxT = document.createElement("span");
+					maxT.classList.add("max-temp");
+					maxT.innerHTML = data.forecast.forecastday[i].day.maxtemp_c + "°C";
+					minMax.appendChild(maxT);
 				}
 			}
-		})
-		.catch(err => {
+		}).catch(err => {
 			//not working
 			// alert("Hey are you sure you are not holding up your map upside down?")
 		});
+}
+
+// add eventlistener to input field
+inputField.addEventListener('keyup', function (event) {
+	if (event.code === "Enter") {
+		startWeatherApp();
+	}
 });
+
+// add eventlistener to buttonElement
+buttonElement.addEventListener('click', startWeatherApp);
 
 // This is a weather web application made for educational purposes. Please do not commercialize this project in any way whatsoever.
 // Made by a BeCode technical coach whom had a lot of fun making "bad code", and improved by the very learners of this class.
