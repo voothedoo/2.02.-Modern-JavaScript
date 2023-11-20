25f46f2f40d976302bb714666764e086b2db562a
Alex M - laptop <vootehdoo@gmail.com>
2023-11-20 14:55:38 +0100

    Replace if statement with a ternary operator

--- scripts/script.js
+++ scripts/script.js
@@ -1,72 +1,71 @@
 import { API } from "./config.js";
 
-const createContainer = (data) => {
-	for (let i = 0; i < 5; i++) {
-		const div = document.querySelector('.container');
+const createContainer = (data, i) => {
+	const div = document.querySelector('.container');
 
-		const daysOfTheWeekArray = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-		const date = new Date();
-		const weekDays = daysOfTheWeekArray[(date.getDay() + i) % 7];
+	const daysOfTheWeekArray = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+	const date = new Date();
+	const weekDays = daysOfTheWeekArray[(date.getDay() + i) % 7];
 
-		const card = document.createElement('div');
-		card.classList.add("card");
-		if (i === 0) card.classList.add("main-card");
-		div.appendChild(card);
+	const card = document.createElement('div');
+	card.classList.add("card");
+	if (i === 0) card.classList.add("main-card");
+	div.appendChild(card);
 
-		const initialContentBeforeSlideAnimation = document.createElement('div');
-		initialContentBeforeSlideAnimation.classList.add("imgBx");
-		card.appendChild(initialContentBeforeSlideAnimation);
+	const initialContentBeforeSlideAnimation = document.createElement('div');
+	initialContentBeforeSlideAnimation.classList.add("imgBx");
+	card.appendChild(initialContentBeforeSlideAnimation);
 
-		const cardImg = document.createElement('img');
-		cardImg.src = data.forecast.forecastday[i].day.condition.icon;
-		cardImg.alt = "Icon describing the following weather: " + data.forecast.forecastday[i].day.condition.text;
-		initialContentBeforeSlideAnimation.appendChild(cardImg);
+	const cardImg = document.createElement('img');
+	cardImg.src = data.forecast.forecastday[i].day.condition.icon;
+	cardImg.alt = "Icon describing the following weather: " + data.forecast.forecastday[i].day.condition.text;
+	initialContentBeforeSlideAnimation.appendChild(cardImg);
 
-		const contentBox = document.createElement("div");
-		contentBox.classList.add("contentBx");
-		card.appendChild(contentBox);
+	const contentBox = document.createElement("div");
+	contentBox.classList.add("contentBx");
+	card.appendChild(contentBox);
 
-		const dowContentBeforeSliderAnimation = document.createElement("h2");
-		dowContentBeforeSliderAnimation.innerHTML = weekDays;
-		contentBox.appendChild(dowContentBeforeSliderAnimation);
+	const dowContentBeforeSliderAnimation = document.createElement("h2");
+	dowContentBeforeSliderAnimation.innerHTML = weekDays;
+	contentBox.appendChild(dowContentBeforeSliderAnimation);
 
-		console.log(data.forecast.forecastday[i].day.condition.text);
-		const tempDescription = document.createElement("h4");
-		tempDescription.innerHTML = data.forecast.forecastday[i].day.condition.text;
-		contentBox.appendChild(tempDescription);
+	console.log(data.forecast.forecastday[i].day.condition.text);
+	const tempDescription = document.createElement("h4");
+	tempDescription.innerHTML = data.forecast.forecastday[i].day.condition.text;
+	contentBox.appendChild(tempDescription);
 
-		const currentTempBox = document.createElement("div");
-		currentTempBox.classList.add("color");
-		contentBox.appendChild(currentTempBox);
+	const currentTempBox = document.createElement("div");
+	currentTempBox.classList.add("color");
+	contentBox.appendChild(currentTempBox);
 
-		const currentTempHeader = document.createElement("h3");
-		currentTempHeader.innerHTML = "Temp:";
-		currentTempBox.appendChild(currentTempHeader);
+	const currentTempHeader = document.createElement("h3");
+	currentTempHeader.innerHTML = "Temp:";
+	currentTempBox.appendChild(currentTempHeader);
 
-		const currentT = document.createElement("span");
-		currentT.classList.add("current-temp");
+	const currentT = document.createElement("span");
+	currentT.classList.add("current-temp");
 
-		currentT.innerHTML = data.forecast.forecastday[i].day.avgtemp_c + "°C";
-		currentTempBox.appendChild(currentT);
+	currentT.innerHTML = data.forecast.forecastday[i].day.avgtemp_c + "°C";
+	currentTempBox.appendChild(currentT);
 
-		const minMax = document.createElement("div");
-		minMax.classList.add("details");
-		contentBox.appendChild(minMax);
+	const minMax = document.createElement("div");
+	minMax.classList.add("details");
+	contentBox.appendChild(minMax);
 
-		const minMaxTempHeader = document.createElement("h3");
-		minMaxTempHeader.innerHTML = "More:";
-		minMax.appendChild(minMaxTempHeader);
+	const minMaxTempHeader = document.createElement("h3");
+	minMaxTempHeader.innerHTML = "More:";
+	minMax.appendChild(minMaxTempHeader);
 
-		const minT = document.createElement("span");
-		minT.classList.add("min-temp");
-		minT.innerHTML = data.forecast.forecastday[i].day.mintemp_c + "°C";
-		minMax.appendChild(minT);
+	const minT = document.createElement("span");
+	minT.classList.add("min-temp");
+	minT.innerHTML = data.forecast.forecastday[i].day.mintemp_c + "°C";
+	minMax.appendChild(minT);
 
-		const maxT = document.createElement("span");
-		maxT.classList.add("max-temp");
-		maxT.innerHTML = data.forecast.forecastday[i].day.maxtemp_c + "°C";
-		minMax.appendChild(maxT);
-	}
+	const maxT = document.createElement("span");
+	maxT.classList.add("max-temp");
+	maxT.innerHTML = data.forecast.forecastday[i].day.maxtemp_c + "°C";
+	minMax.appendChild(maxT);
+
 };
 const removeContainer = () => {
 	const container = document.querySelector(".container");
@@ -85,14 +84,13 @@ const startApp = async () => {
 		return alert("Hey are you sure you are not holding up your map upside down?");
 	}
 	removeContainer();
-	createContainer(data);
-
+	for (let i = 0; i < 5; i++) {
+		createContainer(data, i);
+	}
 	document.querySelector('.city-info').textContent = data.location.name + ", " + data.location.country;
 };
 
 document.querySelector('#city-name').addEventListener('keyup', (event) => {
-	if (event.code === "Enter") {
-		startApp();
-	}
+	(event.code === "Enter") ? startApp() : null;
 });
 document.querySelector('#submit-search').addEventListener('click', startApp);
