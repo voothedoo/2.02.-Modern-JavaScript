92851e08e439a068f01931bf0e39457e07e119bf
Alex Munteanu <vootehdoo@gmail.com>
2023-11-21 15:40:36 +0100

    Move startapp in a new file

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,5 @@
-import { createContainer, emptyContainer } from "./domUtils.js";
-import { getData } from "./apiUtils.js";
+import { startApp } from "./startApp.js";
 
-const startApp = async () => {
-	const data = await getData();
-	if (data.error) return alert("Hey are you sure you are not holding up your map upside down?");
-
-	emptyContainer();
-	for (let i = 0; i < 5; i++) createContainer(data, i);
-
-	document.querySelector('.city-info').textContent = data.location.name + ", " + data.location.country;
-};
 document.querySelector('#city-name').addEventListener('keyup', (event) => {
 	(event.code === "Enter") ? startApp() : null;
 });
